efca24e93e57117ef0504f58c5b9038859277361
Kaio Melo dos Santos <kaiomelo1998@gmail.com>
2023-02-05 12:46:56 -0300

    01.02 Pequenos Ajustes no inicio do projeto

---
+++ src/assets/components/template/Footer/index.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+function Footer() {
+  return (
+    <footer>
+      <p>© Desenvolvido por programadores cariocas</p>
+    </footer>
+  )
+}
+
+export default Footer
---
+++ src/assets/components/template/Navbar/index.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+function Navbar(){
+  return(
+    <nav className='navBar'>
+        <a href="">Início</a>
+        <a href="">Sobre Nós</a>
+        <a href="">Nossos Serviços</a>
+        <a href="">Agende-se</a>
+        <a href="">Contatos</a>
+      </nav>
+  )
+}
+
+export default Navbar
