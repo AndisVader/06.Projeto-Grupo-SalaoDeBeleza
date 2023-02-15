62f0f2eee7930121a147bc2fb89a17a143883e12
Kaio Melo dos Santos <kaiomelo1998@gmail.com>
2023-02-15 14:14:48 -0300

    01.05.01 Ajustando API

--- src/frontend/components/others/ServiceAPI/CabeloAPI.jsx
+++ src/frontend/components/others/ServiceAPI/CabeloAPI.jsx
@@ -5,20 +5,39 @@ const baseURL = "http://localhost:3001/Cabelo";
 
 export default function CabeloAPI() {
   const [post, setPost] = React.useState(null);
+  const [error, setError] = React.useState(null);
 
   React.useEffect(() => {
     axios.get(baseURL).then((response) => {
       setPost(response.data);
+    }).catch(error => {
+      setError(error);
     });
   }, []);
 
-  if (!post) return null;
+  if (error) return `Error: ${error.message}`;
+  if (!post) return "No post!"
+
+  const arr = post.map((post, index) =>{
+    return (
+      <tr>
+        <td>{post.nome}</td>
+        <td>{post.valor}</td>
+        <td>{post.tempo}</td>
+        <td>{post.img}</td>
+      </tr>
+    );
+  })
 
   return (
     <div>
-      <h2>{post.nome}</h2>
-      <p>{post.valor}</p>
-      <span>${post.tempo}</span>
+      <tr>
+        <td>Nome</td>
+        <td>Valor</td>
+        <td>Tempo</td>
+        <td>Imagem</td>
+      </tr>
+      {arr}
     </div>
   );
 }
