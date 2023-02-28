0a16aaaadd0e803b1caead5e5bbfa92f713ca94e
Anderson Assum <anderson.assum12@gmail.com>
2023-02-28 00:09:04 -0300

    Atualização

--- src/frontend/pages/Home/Home.jsx
+++ src/frontend/pages/Home/Home.jsx
@@ -2,9 +2,38 @@ import React from 'react'
 import Main from '../../components/template/Main/Main'
 import './Home.css'
 import Banner from './Banner'
+import { GiHairStrands } from 'react-icons/gi'
+import menu from '../../assets/img/menu.jpg'
+import menu2 from '../../assets/img/menu2.jpg'
+import menu3 from '../../assets/img/menu3.jpg'
+import MapPage from '../Map/MapPage'
 
 export default props =>
   <Main>
-    <Banner/>
+    <Banner />
+    <section className='aboutUs'>
+      <h1 className='text2'>Seja bem vindo aos Fofinhas Hair. <br />
+        Esse salão de beleza é feito para você que quer sair do padrão. Temos as melhores variações e estilos para seu corte de cabelo.
+        Se diferencie, utilize cores vibrantes e se destaque nas multidões</h1>
+      <GiHairStrands className='icon' />
+      {/*aqui eu planejo deixar uma animação*/}
+    </section>
+    <section className='BotaoDeServico'>
+      <h1 className='servicos'>venha voce conhecer nossos cortes
+      </h1>
+    </section>
+    <section className='pictures'>
+      <img src={menu} />
+      <img src={menu2} />
+      <img src={menu3} />
+      {/* serao 3 fotos que será cabelo, spa, tratamentos. preciso lembrar do background maneiro*/}
+    </section>
+    <section className='avaliacoes'>
+      {/*avaliacoes do google*/}
+    </section>
+    <section className='local'>
+      <MapPage />
+
+      {/*visite nos nesse lugar*/}
+    </section>
   </Main>
-  
